3ff412ac64cb4b4dff447cee5e4c682e8e42ca0d
BillCI <bill@concreteinteractive.com>
2015-04-21 09:50:25 -0700

    figured out why tghe test whernt running. it was because i didnt specify a class for the factory.

--- spec/factories/documentations.rb
+++ spec/factories/documentations.rb
@@ -1,20 +1,20 @@
 FactoryGirl.define do
-  factory :valid_test_doc do
+  factory :valid_test_doc, class: Documentation do
     title "Test Document"
     body "Here we test the body"
     version 1.0
   end
 
-  factory :no_title_doc do
+  factory :no_title_doc, class: Documentation do
     body "Here we test the body"
     version 1.0
   end
-  factory :no_version_doc do
+  factory :no_version_doc, class: Documentation do
     title "Test Document"
     body "Here we test the body"
   end
 
-  factory :no_body_doc do
+  factory :no_body_doc, class: Documentation do
     title "Test Document"
     version 1.0
   end
